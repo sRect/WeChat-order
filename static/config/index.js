d98c498d8eb08cb9558339a1c9a2d35dd8fdc0c4
sRect <fcqun@sina.cn>
2018-02-07 18:15:08 +0800

    接口URL配置

---
+++ static/config/index.js
@@ -0,0 +1,9 @@
+const AXIOSURl = 'https://www.jzwms.com'; // 接口baseURL
+// const AXIOSURl = 'http://daili.ehaofang.com:8099'; // 正式接口baseURL
+// const PUBLICURL = 'http://192.168.1.186/efmanage/'; // FooterComponent拼接路径
+// const PUBLICURL = 'http://guanjia.ehaofang.com/efmanage/'; // 正式FooterComponent拼接路径
+
+export default {
+  "AXIOSURl": AXIOSURl,
+  // "PUBLICURL": PUBLICURL
+}
